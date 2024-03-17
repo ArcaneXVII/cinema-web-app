5d41edf9a00897f09f2317d77ea19e4ebd18410e
sakurv <sakurv@ttu.ee>
2024-03-17 20:03:11 +0200

    Implemented Screening and Reservation tables with required fields. Film rating column will follow MPA film rating system.

---
+++ Backend/src/main/java/ee/arcane/cinemawebapp/repository/ReservationRepository.java
@@ -0,0 +1,7 @@
+package ee.arcane.cinemawebapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+}
